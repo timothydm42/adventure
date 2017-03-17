14d305bdbe70c31c873671347d0d7ea81f9c96c0
Timothy Daniel Mitchell <m.timothyd@gmail.com>
2017-03-17 17:41:05 -0600

    keep going

--- js/directives/homeDir.js
+++ js/directives/homeDir.js
@@ -6,13 +6,15 @@ angular.module("app").directive("home", [function() {
     restrict: "E",
     scope: {},
     controller: ["$scope", "service", function($scope, service) {
+      
 
-      function getSpecies() {
-        service.getSpecies().then(function(result) {
-          $scope.species = result.data
-        })
-      }
-      getSpecies();
+      // function getSpecies() {
+      //   service.getSpecies().then(function(result) {
+      //     console.log(result);
+      //     $scope.species = result.data.flavor_text_entries[1].flavor_text;
+      //   })
+      // }
+      // getSpecies();
 
     }],
   };
